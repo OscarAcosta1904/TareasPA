0533e1a42992699c5577a586e128d73b8063248f
oscar <osc.12321@gmail.com>
2024-09-20 22:45:45 -0600

    tarea 9

---
+++ mindbox/materias/materia.py
@@ -0,0 +1,6 @@
+class Materia:
+    id: int #"MT{ultimos 2 digitos del nombre}{semestre}{cantidad creditos}{random(1, 1000)}": 
+    nombre: str
+    descripcion: str
+    semestre: int
+    creditos: int
